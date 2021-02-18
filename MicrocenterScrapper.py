aa06001fc2be6a0d6d136bf40a14701cc0ce96ea
warrenbr <lugnutz99@gmail.com>
2021-02-17 19:45:49 -0700

    added array ability to checker for multiple product pages

--- MicrocenterScrapper.py
+++ MicrocenterScrapper.py
@@ -11,7 +11,22 @@ class MicrocenterScrapper(object):
         user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0'
         self.header = {'User-Agent': user_agent}
         #product page that you wish to refresh to check stock
-        self.URLRequest = "https://www.microcenter.com/product/628318/asus-geforce-rtx-3080-tuf-gaming-overclocked-triple-fan-10gb-gddr6x-pcie-40-graphics-card?storeid=121"
+        self.URLRequests = ["https://www.microcenter.com/product/628318/asus-geforce-rtx-3080-tuf-gaming-overclocked-triple-fan-10gb-gddr6x-pcie-40-graphics-card?storeid=121", #asus tuff
+                            "https://www.microcenter.com/product/631533/msi-geforce-rtx-3080-suprim-x-overclocked-triple-fan-10gb-gddr6x-pcie-40-graphics-card?storeid=121", #MSI GeForce RTX 3080 SUPRIM X
+                            "https://www.microcenter.com/product/628330/msi-geforce-rtx-3080-gaming-x-trio-triple-fan-10gb-gddr6x-pcie-40-graphics-card?storeid=121", #MSI GeForce RTX 3080 Gaming X Trio
+                            "https://www.microcenter.com/product/628343/gigabyte-geforce-rtx-3080-eagle-overclocked-triple-fan-10gb-gddr6x-pcie-40-graphics-card?storeid=121", #Gigabyte GeForce RTX 3080 Eagle Overclocked Triple-Fan
+                            "https://www.microcenter.com/product/628346/evga-geforce-rtx-3080-ftw3-ultra-gaming-triple-fan-10gb-gddr6x-pcie-40-graphics-card?storeid=121", #EVGA GeForce RTX 3080 FTW3 Ultra Gaming Triple-Fan
+                            "https://www.microcenter.com/product/628686/asus-geforce-rtx-3080-strix-overclocked-triple-fan-10gb-gddr6x-pcie-40-graphics-card?storeid=121", #ASUS GeForce RTX 3080 Strix Overclocked Triple-Fan
+                            "https://www.microcenter.com/product/628331/msi-geforce-rtx-3080-ventus-3x-overclocked-triple-fan-10gb-gddr6x-pcie-40-graphics-card?storeid=121"] #MSI GeForce RTX 3080 Ventus 3X Overclocked Triple-Fan
+
+        self.ProductName = ["Asus Tuff",
+                            "MSI GeForce RTX 3080 SUPRIM X",
+                            "MSI GeForce RTX 3080 Gaming X Trio",
+                            "Gigabyte GeForce RTX 3080 Eagle Overclocked Triple-Fan",
+                            "EVGA GeForce RTX 3080 FTW3 Ultra Gaming Triple-Fan",
+                            "ASUS GeForce RTX 3080 Strix Overclocked Triple-Fan",
+                            "MSI GeForce RTX 3080 Ventus 3X Overclocked Triple-Fan"]
+
         #location of the alert
         self.NOTIFICATION_SOUND_PATH = "Notification/alarm-frenzy-493.mp3"
         #sold out state of text
@@ -25,22 +40,25 @@ class MicrocenterScrapper(object):
 
     def CheckForStock(self):
         try:
-            with requests.get(self.URLRequest, headers=self.header) as url:
-                tree = html.fromstring(url.content)
-                #the xpath of the text object in the HTML page that will be checked
-                stockText = tree.xpath("/html/body/main/article/div[3]/div[1]/div[1]/div/div[2]/div[1]/p/span/text()")[0]
-                
-                print("at " + str(datetime.now()) +  " sock text is: " + str(stockText))
-                #Checks the text of the object, this hardcode string must be the exact sold out text
-            if(str(stockText) != self.soldOutStockText):
-                self.inStock = True
-                print("at " + str(datetime.now()) + " ######################################STOCK FOUND######################################")
-                # This is the In stock condition! Modify this line if you wish to have a different location. Plays terrible audio x times in a row
-                for num in range(10):
-                    self.PlayAudio(self.NOTIFICATION_SOUND_PATH)
+            for productNum in range(len(self.URLRequests)):
+                with requests.get(self.URLRequests[productNum], headers=self.header) as url:
+                    tree = html.fromstring(url.content)
+                    #the xpath of the text object in the HTML page that will be checked
+                    stockText = tree.xpath("/html/body/main/article/div[3]/div[1]/div[1]/div/div[2]/div[1]/p/span/text()")[0]
+                    
+                    print("at " + str(datetime.now()) + " for " + self.ProductName[productNum] + " sock text is: " + str(stockText))
+                    #Checks the text of the object, this hardcode string must be the exact sold out text
+                if(str(stockText) != self.soldOutStockText):
+                    self.inStock = True
+                    print("at " + str(datetime.now()) + " ######################################STOCK FOUND######################################")
+                    print("For " + self.ProductName[productNum])
+                    # This is the In stock condition! Modify this line if you wish to have a different location. Plays terrible audio x times in a row
+                    for num in range(10):
+                        self.PlayAudio(self.NOTIFICATION_SOUND_PATH)
 
         except Exception as e:
-            print("network or scrapping error!   " + e)
+            print("Network or scraping error in CheckForStock()")
+            print(e)
 
 
     def PlayAudio(self, audio_file=None, **kwargs):
